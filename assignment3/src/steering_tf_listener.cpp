f5eadad20ea926ce466da052d86be363328755d4
viki <bilalbutt.614@gmail.com>
2017-11-01 14:36:55 +0100

    solution for quad

--- assignment3/src/steering_tf_listener.cpp
+++ assignment3/src/steering_tf_listener.cpp
@@ -3,6 +3,14 @@
 #include <geometry_msgs/Twist.h>
 #include <turtlesim/Spawn.h>
 #include "steering.hpp"
+#include <math.h>
+
+const double lookaheadRadius = 3.0;
+
+double getSlope_L(double x1, double x2, double y1, double y2);//returns the slope of a line, constructed from 2 given points
+double getIntercept_L(double x, double y, double slope);//returns the y-intercept value of a line, given a point and slope
+double solveCircleLineQuad(double x1, double x2, double y1, double y2);//Solve for intersection points of the line L defined by 
+//2 points and the circle C with radius defined by lookaheadRadius
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "local_planner");
@@ -32,25 +40,78 @@ int main(int argc, char** argv){
       continue;
     }
 		
-	/*
 	ROS_INFO_STREAM(std::setprecision(2) << std::fixed
 		<< "\nMap Message: x-position: " << transform.getOrigin().x()
 		<<", y-position:" << transform.getOrigin().y());
-	*/
+	
+	
+	//x and y represent current position of the robot, th represents orientation of the robot	
+	double x = position_transform.getOrigin().x();
+	double y = position_transform.getOrigin().y();
+	double th = tf::getYaw(position_transform.getRotation());
+	
 	ROS_INFO_STREAM(std::setprecision(2) << std::fixed
-		<< "\nCurrent Position: x-position: " << position_transform.getOrigin().x()
-		<<", y-position:" << position_transform.getOrigin().y());
+		<< "\nCurrent Position: x-position: " << x
+		<<", y-position:" << y
+		<< ", rotation: " << th);
 	
 	//steering.setGoal(transform.getOrigin().x(), transform.getOrigin().y(), M_PI);
     geometry_msgs::Twist vel_msg;
     
-                                 
+    float deltaX = transform.getOrigin().x() - position_transform.getOrigin().x();
+    float deltaY = transform.getOrigin().y() - position_transform.getOrigin().y();
+    
+    float rho = sqrt(pow(deltaX, 2) + pow(deltaY, 2));
+        
     vel_msg.linear.x = 0.1;
+    //vel_msg.angular.z = 0.1;
     stage_vel.publish(vel_msg);
     rate.sleep();
-    
 	
   }
   return 0;
 };
 
+double getSlope_L(double x1, double x2, double y1, double y2)
+{
+	return (y2 - y1)/(x2 - x1);
+}
+
+double getIntercept_L(double x, double y, double slope)
+{
+	return y - slope*x;
+}
+
+double solveCircleLineQuad(double x1, double x2, double y1, double y2)
+{
+
+	double m = getSlope_L(x1, x2, y1, y2);
+	double B = getIntercept_L(x1, y1, m);
+	double a = 1 + pow(m, 2);
+	double b = 2*B*m;
+	double c = pow(B, 2) - pow(lookaheadRadius, 2);
+	
+	if(a<0.000001)    // ==0
+	{
+		if(b>0.000001)  // !=0
+			result.p1.r=result.p2.r=-c/b;
+		else
+			if(c>0.00001) throw exception("no solutions");
+		return result;
+	}
+
+	double delta=b*b-4*a*c;
+	if(delta>=0)
+	{
+		result.p1.r=(-b-sqrt(delta))/2/a;
+		result.p2.r=(-b+sqrt(delta))/2/a;
+	}
+	else
+	{
+		result.p1.r=result.p2.r=-b/2/a;
+		result.p1.i=sqrt(-delta)/2/a;
+		result.p2.i=-sqrt(-delta)/2/a;
+	}
+
+	return result;
+}
