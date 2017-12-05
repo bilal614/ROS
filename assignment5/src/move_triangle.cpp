db223c7f0fa851b055807888560ab9d47760006b
thanhhnk <thanhhnk@gmail.com>
2017-12-05 13:53:47 +0100

    Publisher for triangle message via /cmd topic + note

--- assignment5/src/move_triangle.cpp
+++ assignment5/src/move_triangle.cpp
@@ -2,6 +2,7 @@
 #include <actionlib/client/simple_action_client.h>
 #include <actionlib/client/terminal_state.h>
 #include <turtlebot_actions/TurtlebotMoveAction.h>
+#include <assignment5/Triangle.h>
 
 int main(int argc, char **argv)
 {
