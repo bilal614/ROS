d22771050852c7e6a4195095805115ed2b52b61e
bilal614 <bilalbutt.614@gmail.com>
2017-12-11 18:33:27 +0100

    first static triangle drawn

--- assignment1/src/traveler_main.cpp
+++ assignment1/src/traveler_main.cpp
@@ -1,6 +1,6 @@
 #include "traveler.hpp"
 
-	
+
 
 int main(int argc, char **argv)
 {	
--- assignment5/src/move_triangle.cpp
+++ assignment5/src/move_triangle.cpp
@@ -4,16 +4,26 @@
 #include <turtlebot_actions/TurtlebotMoveAction.h>
 #include <assignment5/Triangle.h>
 
+void TriangleMessageCallback(const assignment5::Triangle& msg);
+bool triangle_trigger = false;
+assignment5::Triangle triangleDrawing;
 
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "test_turtle");
+	
+	//ros::NodeHandle nh;
+	//ros::Subscriber subs = nh.subscribe("triangles", 1000, &TriangleMessageCallback);
 
 	// create the action client
 	// true causes the client to spin its own thread
 	actionlib::SimpleActionClient<turtlebot_actions::TurtlebotMoveAction> ac(
 			"turtlebot_move", true);
-
+	/*
+	while(!triangle_trigger)
+	{
+	}
+	*/
 	ROS_INFO("Waiting for action server to start.");
 	// wait for the action server to start
 	ac.waitForServer(); //will wait for infinite time
@@ -23,24 +33,37 @@ int main(int argc, char **argv)
 	turtlebot_actions::TurtlebotMoveGoal goalAction;
 	//TODO investiage the goal message of turtle_bot actions
 
-	goalAction.forward_distance = 4.0f;
-	goalAction.turn_distance = M_PI/4;
-
-	ac.sendGoal(goalAction);
-	
-	//actionlib::SimpleClientGoalState state = ac.sendGoalAndWait(goalAction, ros::Duration(30.0));
-	//wait for the action to return
-	bool finished_before_timeout = ac.waitForResult(ros::Duration(30.0));
-
-	if (finished_before_timeout)
+	int count = 0;
+	while(count < 3)
 	{
-		actionlib::SimpleClientGoalState state = ac.getState();
-		ROS_INFO("Action finished: %s",state.toString().c_str());
-	}
-	else
-		ROS_INFO("Action did not finish before the time out.");
+		goalAction.forward_distance = 4.0f;
+		goalAction.turn_distance = M_PI*2/3;
 
-	usleep(5000000);
+		ac.sendGoal(goalAction);
+		
+		//actionlib::SimpleClientGoalState state = ac.sendGoalAndWait(goalAction, ros::Duration(30.0));
+		//wait for the action to return
+		bool finished_before_timeout = ac.waitForResult(ros::Duration(30.0));
+
+		if (finished_before_timeout)
+		{
+			actionlib::SimpleClientGoalState state = ac.getState();
+			ROS_INFO("Action finished: %s",state.toString().c_str());
+		}
+		else
+			ROS_INFO("Action did not finish before the time out.");
+			
+		count++;
+	}
 	//exit
 	return 0;
+}
+
+void TriangleMessageCallback(const assignment5::Triangle& msg)
+{
+	ROS_INFO_STREAM(std::setprecision(2) << std::fixed
+		<< "Message: " << msg);
+		
+	triangleDrawing.sideLength = msg.sideLength;
+	triangleDrawing.cw = msg.cw;
 }
