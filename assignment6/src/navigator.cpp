57e9eeb7746946691ad1be09118723ea96977374
bilal614 <bilalbutt.614@gmail.com>
2018-01-08 11:54:55 +0100

    prepared launch file with other launch file included

--- assignment6/src/navigator.cpp
+++ assignment6/src/navigator.cpp
@@ -3,36 +3,88 @@
 #include <actionlib/client/simple_action_client.h>
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
+void GetFloatParam(std::string paramName, float* value);
+const int NrOfCoordinates = 4;
 
-int main(int argc, char** argv){
-  ros::init(argc, argv, "simple_navigation_goals");
+int main(int argc, char** argv)
+{
+	ros::init(argc, argv, "simple_navigation_goals");
 
-  //tell the action client that we want to spin a thread by default
-  MoveBaseClient ac("move_base", true);
+	//tell the action client that we want to spin a thread by default
+	MoveBaseClient ac("move_base", true);
 
-  //wait for the action server to come up
-  while(!ac.waitForServer(ros::Duration(5.0))){
-    ROS_INFO("Waiting for the move_base action server to come up");
-  }
+	//wait for the action server to come up
+	while(!ac.waitForServer(ros::Duration(5.0))){
+		ROS_INFO("Waiting for the move_base action server to come up");
+	}
 
-  move_base_msgs::MoveBaseGoal goal;
+	move_base_msgs::MoveBaseGoal goal;
 
-  //we'll send a goal to the robot to move 1 meter forward
-  goal.target_pose.header.frame_id = "base_link";
-  goal.target_pose.header.stamp = ros::Time::now();
+	int counter = 0;
+	while(counter < NrOfCoordinates)
+	{
+		//we'll send a goal to the robot to move 1 meter forward
+		goal.target_pose.header.frame_id = "base_link";
+		goal.target_pose.header.stamp = ros::Time::now();
+		float x[NrOfCoordinates] = {0.0};
+		float y[NrOfCoordinates] = {0.0};
+		float w[NrOfCoordinates] = {0.0};
+		switch(counter)
+		{
+			case 0:
+				GetFloatParam("poseX1", &x[counter]);
+				GetFloatParam("poseY1", &y[counter]);
+				GetFloatParam("poseW1", &w[counter]);
+				break;
+			case 1:
+				GetFloatParam("poseX2", &x[counter]);
+				GetFloatParam("poseY2", &y[counter]);
+				GetFloatParam("poseW2", &w[counter]);
+				break;
+			case 2:
+				GetFloatParam("poseX3", &x[counter]);
+				GetFloatParam("poseY3", &y[counter]);
+				GetFloatParam("poseW3", &w[counter]);
+				break;
+			case 3:
+				GetFloatParam("poseX4", &x[counter]);
+				GetFloatParam("poseY4", &y[counter]);
+				GetFloatParam("poseW4", &w[counter]);
+				break;
+			default:
+				break;
+		}
+		goal.target_pose.pose.position.x = x[counter];
+		goal.target_pose.pose.position.y = y[counter];
+		goal.target_pose.pose.orientation.w = w[counter];
+		//goal.target_pose.pose.position.x = 1.0;
+		//goal.target_pose.pose.position.y = -1.0;
+		//goal.target_pose.pose.orientation.w = 1.0;
 
-  goal.target_pose.pose.position.x = 1.0;
-  goal.target_pose.pose.orientation.w = 1.0;
+		ROS_INFO("Sending goal");
+		ac.sendGoal(goal);
 
-  ROS_INFO("Sending goal");
-  ac.sendGoal(goal);
+		ac.waitForResult();
 
-  ac.waitForResult();
+		if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+			ROS_INFO("Hooray, the base moved to your goal.");
+		else
+			ROS_INFO("The base failed to move to your goal for some reason");
+		counter++;
+	}
+	return 0;
+}
 
-  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    ROS_INFO("Hooray, the base moved 1 meter forward");
-  else
-    ROS_INFO("The base failed to move forward 1 meter for some reason");
 
-  return 0;
+void GetFloatParam(std::string paramName, float* value)
+{
+	const std::string PARAM_NAME = paramName;
+	bool ok = ros::param::get(PARAM_NAME, *value);
+	if (!ok)
+	{
+		ROS_FATAL_STREAM("Could not get parameter " << PARAM_NAME);
+		exit(1);
+	}
+	ROS_INFO_STREAM(
+			std::setprecision(2) << std::fixed << "\n" <<paramName << ": " << *value);
 }
