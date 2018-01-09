3cdccdc42b0f09a4c3648213416477cccf5de66d
bilal614 <bilalbutt.614@gmail.com>
2018-01-09 23:08:48 +0100

    working on building arbiter class

--- mines-ros/stage_behavior/src/arbiter.cpp
+++ mines-ros/stage_behavior/src/arbiter.cpp
@@ -1,5 +1,6 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
+#include "stdint.h"
 
 /**
  * Priority based arbiter, forwards highest priority commands
@@ -30,55 +31,91 @@ protected:
     };
     
 	// members
-	ros::NodeHandle nh_;
+	ros::NodeHandle nh_, ph_;
+	ros::Subscriber sub_0, sub_1, sub_2;
 	ros::Publisher vel_pub_;
-	geometry_msgs::Twist cmd_vel_;
+	
+	int rate_; // update and publish rate (Hz)
+	int inputs_;
+	
+	geometry_msgs::Twist cmd_vel_tele_op, cmd_vel_escape, cmd_vel_cruise;
 	Priority priority_;
-	u_int8_t priority_mask_;
+	unsigned char priority_mask_;
 	// methods
 };
 
 ///////////////////////////////////////////////////////////////////////////
 
-Arbiter::Arbiter(): priority_mask_(0)
+Arbiter::Arbiter(): ph_("~"), rate_(1), inputs_(3)
 {
-	ros::Subscriber sub = n.subscribe("cmd_vel0", 1, teleopCallback);
-	ros::Subscriber sub = n.subscribe("cmd_vel1", 1, escape_behaviorCallback);
-	ros::Subscriber sub = n.subscribe("cmd_vel2", 1, cruise_behaviorCallback);
+	ph_.param("publish_rate", rate_, rate_);
+	ph_.param("inputs", inputs_, inputs_);
 	
-	vel_pub_ = nh_.advertise<geometry_msgs::Twist>("cmd_vel", 1);
+	priority_mask_ = 0x00;
+	ros::Subscriber sub_0 = nh_.subscribe("/cmd_vel0", 1, &Arbiter::teleopCallback, this);
+	ros::Subscriber sub_1 = nh_.subscribe("/cmd_vel1", 1, &Arbiter::escape_behaviorCallback, this);
+	ros::Subscriber sub_2 = nh_.subscribe("/cmd_vel2", 1, &Arbiter::cruise_behaviorCallback, this);
+	
+	vel_pub_ = nh_.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
+	
+	ROS_INFO_STREAM(std::setprecision(2) << std::fixed << "Arbiter constructor is called");
+}
+
+void Arbiter::publish(double angular, double linear)
+{
+	geometry_msgs::Twist vel;
+	vel.angular.z = angular;
+	vel.linear.x = linear;
+	vel_pub_.publish(vel);    
+	return;
 }
 
 void Arbiter::teleopCallback(const geometry_msgs::Twist &msg)
 {
 	priority_mask_ |= 0x01;
+	cmd_vel_tele_op.angular.z = msg.angular.z;
+	cmd_vel_tele_op.linear.x = msg.linear.x;
+	return;
 }
 
 void Arbiter::escape_behaviorCallback(const geometry_msgs::Twist &msg)
 {
-	priority_mask_ |= 0x02;	
+	priority_mask_ |= 0x02;
+	cmd_vel_escape.angular.z = msg.angular.z;
+	cmd_vel_escape.linear.x = msg.linear.x;
+	return;
 }
 
 void Arbiter::cruise_behaviorCallback(const geometry_msgs::Twist &msg)
 {
+	ROS_INFO_STREAM(std::setprecision(2) << std::fixed
+		<< "msg: " << msg);
 	priority_mask_ |= 0x04;	
+	cmd_vel_cruise.angular.z = msg.angular.z;
+	cmd_vel_cruise.linear.x = msg.linear.x;
+	return;
 }
 
 void Arbiter::setPriority()
 {
+	//ROS_INFO_STREAM(std::setprecision(2) << std::fixed
+		//<< "priority_mask: " << (int)priority_mask_);
 	if(priority_mask_ & 0x01)
 	{
 		priority_ = TELE_OP;
+		publish(cmd_vel_tele_op.angular.z, cmd_vel_tele_op.linear.x);
 		return;
 	}
 	else if(priority_mask_ & 0x02)
 	{
 		priority_ = ESCAPE;
+		publish(cmd_vel_escape.angular.z, cmd_vel_escape.linear.x);
 		return;
 	}
 	else if(priority_mask_ & 0x04)
 	{
 		priority_ = CRUISE;
+		publish(cmd_vel_cruise.angular.z, cmd_vel_cruise.linear.x);
 		return;
 	}
 	else
@@ -87,11 +124,24 @@ void Arbiter::setPriority()
 	}
 }
 
+void Arbiter::arbitrate()
+{
+	while(ros::ok())
+	{
+		ros::spinOnce();
+		
+		setPriority();
+	}
+}
+
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "arbiter");
   Arbiter arbiter;
-  ros::spin();
+  
+  arbiter.arbitrate();
+ 
+  //ros::spin();
 
   return 0;
 }
